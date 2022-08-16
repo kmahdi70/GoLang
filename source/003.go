0f13b1187039aa39edfc157d20db1463556c4384
Mahdi Karimian <kmahdi70@gmail.com>
2022-08-16 23:01:14 +0430

    timer

--- source/003.go
+++ source/003.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
-	// "os"
+	"strings"
+	"time"
 )
 
 func main() {
 	// arr := [5] string {1:"aaaa",2:"fff",3:"fgg"} 
+	start := time.Now()
 	for k,v:=range os.Args{
 		// fmt.Println("k=",k,",v=",v)
 		fmt.Printf("k=%d,v=%s(%T,%T)\n",k,v,k,v)
 	}
 	// fmt.Println(arr)
+	duration := time.Since(start)
+	fmt.Println("Time1 =",duration)
+	start = time.Now()
+	fmt.Println(strings.Join(os.Args,", "))
+	duration = time.Since(start)
+	fmt.Println("Time2 =",duration)
 }
