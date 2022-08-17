e634d72256986dbc3beb29155a71e2d188cabf95
Mahdi Karimian <kmahdi70@gmail.com>
2022-08-17 08:58:37 +0430

    duration

--- source/003.go
+++ source/003.go
@@ -8,13 +8,10 @@ import (
 )
 
 func main() {
-	// arr := [5] string {1:"aaaa",2:"fff",3:"fgg"} 
 	start := time.Now()
 	for k,v:=range os.Args{
-		// fmt.Println("k=",k,",v=",v)
 		fmt.Printf("k=%d,v=%s(%T,%T)\n",k,v,k,v)
 	}
-	// fmt.Println(arr)
 	duration := time.Since(start)
 	fmt.Println("Time1 =",duration)
 	start = time.Now()
