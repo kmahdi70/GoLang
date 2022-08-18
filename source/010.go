440f2c012dea53e834e82dc6394c79b03156bcd6
Mahdi Karimian <kmahdi70@gmail.com>
2022-08-18 14:34:42 +0430

    Dup - counting duplicated strings

--- source/010.go
+++ source/010.go
@@ -7,10 +7,15 @@ import (
 )
 
 func main() {
+	fmt.Print("\033[H\033[2J")
+	fmt.Println("Enter String and 0 to exit:")
 	counts := make(map[string]int)
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
 		counts[input.Text()]++
+		if input.Text() == "0" {
+			break
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 	for line, n := range counts {
